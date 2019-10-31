c094598b21f6b011dec3ba9e16d9e783ae2f2c04
Alfa <learnpal29@gmail.com>
2019-10-31 09:10:39 +0800

    10 POST api Product

---
+++ server/routes/product.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const Product = require("../models/product");
+// POST request - create a new product
+
+// title: String,
+//   description: String,
+//   photo: String,
+//   price: Number,
+//   stockQuantity: Number,
+//   rating: [Number]
+
+router.post("/products", async (req, res) => {
+  try {
+    let product = new Product();
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.photo = req.body.photo;
+    product.stockQuantity = req.body.stockQuantity;
+
+    await product.save(); // async
+
+    res.json({
+      status: true,
+      message: "Successfully saved"
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+// GET requesst - get all products
+
+// GET request - get a single product
+
+// PUT request - Update a single product
+
+// DELETE request - delete a single product
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -27,26 +27,9 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// GET - Retrieve data from the server
-app.get("/", (req, res) => {
-  res.json("Hello amazon");
-});
-
-// POST - send data from frontend to backend
-app.post("/", (req, res) => {
-  let user = new User();
-  user.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-
-  user.save(err => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json("successfully saved");
-    }
-  });
-});
+// require apis
+const productRoutes = require("./routes/product");
+app.use("/api", productRoutes);
 
 app.listen(3000, err => {
   if (err) {
