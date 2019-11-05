81173fb054aeeafb2c108e31d5ac857f6108987c
Alfa <learnpal29@gmail.com>
2019-11-05 17:01:42 +0800

    34 and 35 Setup consumer facing app & configure css files

---
+++ client/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+    <div>
+      <logo />
+      <h1 class="title">client</h1>
+      <h2 class="subtitle">My first-class Nuxt.js project</h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+</style>
