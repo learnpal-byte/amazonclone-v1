2313471a8418dbe9a46f0fc39fccdaeda336c1db
Alfa <learnpal29@gmail.com>
2019-11-11 20:36:34 +0800

    53 Signup Page Part 1: HTML

---
+++ client/layouts/none.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  font-size: 13px;
+  line-height: 19px;
+  color: #111;
+}
+</style>
---
+++ client/pages/signup.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="registerPage">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
+          <div class="text-center">
+            <a href="#">
+              <img src="/img/logo-black.png" />
+            </a>
+          </div>
+
+          <form class="mt-4">
+            <div class="a-box a-spacing-extra-large">
+              <div class="a-box-inner">
+                <h1 class="a-spacing-small">Create account</h1>
+                <!-- Your Name -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label">Your name</label>
+                  <input
+                    type="text"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                </div>
+                <!-- Email -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label">Email</label>
+                  <input
+                    type="email"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                </div>
+                <!-- Password -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label">Password</label>
+                  <input
+                    type="password"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                  <div class="a-alert-container">
+                    <div class="a-alert-content">Password must be at least 6 characteres</div>
+                  </div>
+                </div>
+                <!-- Button -->
+                <div class="a-row a-spacing-extra-large mb-4">
+                  <span class="a-button-primary">
+                    <span class="a-button-inner">
+                      <span class="a-button-text">Create your Amazon account</span>
+                    </span>
+                  </span>
+                  <div class="a-row a-spacing-top-medium a-size-small">
+                    <b>
+                      By creating an account, you agree to Amazon's
+                      <a href="#">Conditions of Use</a> and
+                      <a href="#">Privacy Notice</a>
+                    </b>
+                  </div>
+                </div>
+                <hr />
+                <div class="a-row">
+                  <b>
+                    Already have an account?
+                    <nuxt-link to="/login" class="a-link-emphasis">Sign in</nuxt-link>
+                  </b>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "none"
+};
+</script>
