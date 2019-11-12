a049a5be9939dfbe04b5553aab5e47de26a1c113
Alfa <learnpal29@gmail.com>
2019-11-13 04:35:08 +0800

    55 If condition User's data on Navbar

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -73,18 +73,31 @@
                 </span>
               </a>
               <span class="icp-nav-link-border"></span>
-              <nuxt-link
-                to="/register"
-                class="nav-a nav-a-2"
-                id="nav-link-accountList"
-                tabindex="0"
-              >
-                <span class="nav-line-1">Hello, Sign in</span>
-                <span class="nav-line-2">
-                  Account &amp; Lists
-                  <span class="nav-icon nav-arrow" style="visibility: visible"></span>
-                </span>
-              </nuxt-link>
+              <template v-if="$auth.$state.loggedIn">
+                <nuxt-link
+                  to="/register"
+                  class="nav-a nav-a-2"
+                  id="nav-link-accountList"
+                  tabindex="0"
+                >
+                  <span class="nav-line-1">Hello,</span>
+                  <span class="nav-line-2">{{ $auth.$state.user.name }}</span>
+                </nuxt-link>
+              </template>
+              <template v-else>
+                <nuxt-link
+                  to="/register"
+                  class="nav-a nav-a-2"
+                  id="nav-link-accountList"
+                  tabindex="0"
+                >
+                  <span class="nav-line-1">Hello, Sign in</span>
+                  <span class="nav-line-2">
+                    Account &amp; Lists
+                    <span class="nav-icon nav-arrow" style="visibility: visible"></span>
+                  </span>
+                </nuxt-link>
+              </template>
               <nuxt-link to="/orders" class="nav-a nav-a-2 nav-single-row-link">
                 <span class="nav-line-1"></span>
                 <span class="nav-line-2">Orders</span>
