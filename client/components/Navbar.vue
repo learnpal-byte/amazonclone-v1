b7307134d4aa063439dada6d590547add84dcf85
Alfa <learnpal29@gmail.com>
2019-11-13 18:29:29 +0800

    57 Profile Page

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -6,9 +6,9 @@
           <!-- Logo -->
           <div class="col-sm-2">
             <div class="logo-area">
-              <a href="#">
+              <nuxt-link to="/">
                 <img src="/img/logo.png" alt="Logo" class="img-fluid" />
-              </a>
+              </nuxt-link>
             </div>
           </div>
           <!-- Search bar -->
@@ -75,7 +75,7 @@
               <span class="icp-nav-link-border"></span>
               <template v-if="$auth.$state.loggedIn">
                 <nuxt-link
-                  to="/register"
+                  to="/profile"
                   class="nav-a nav-a-2"
                   id="nav-link-accountList"
                   tabindex="0"
@@ -86,7 +86,7 @@
               </template>
               <template v-else>
                 <nuxt-link
-                  to="/register"
+                  to="/signup"
                   class="nav-a nav-a-2"
                   id="nav-link-accountList"
                   tabindex="0"
---
+++ client/pages/profile.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <div class="container-fluid c-section">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <div class="a-spacing-top-medium"></div>
+          <h2>Profile Page</h2>
+          <form>
+            <!-- Name -->
+            <div class="a-spacing-top-medium">
+              <label>Name</label>
+              <input
+                class="a-input-text"
+                style="width: 100%"
+                v-model="name"
+                :placeholder="$auth.$state.user.name"
+              />
+            </div>
+
+            <!-- Email -->
+            <div class="a-spacing-top-medium">
+              <label>Email</label>
+              <input
+                class="a-input-text"
+                style="width: 100%"
+                v-model="email"
+                :placeholder="$auth.$state.user.email"
+              />
+            </div>
+
+            <!-- Password -->
+            <div class="a-spacing-top-medium">
+              <label>Password</label>
+              <input class="a-input-text" style="width: 100%" v-model="password" />
+            </div>
+
+            <!-- Button -->
+            <hr />
+            <div class="a-spacing-top-large">
+              <span class="a-button-register">
+                <span class="a-button-inner">
+                  <span class="a-button-text" @click="onUpdateProfile">Update Profile</span>
+                </span>
+              </span>
+            </div>
+          </form>
+          <br />
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </main>
+</template>
+    
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: ""
+    };
+  }
+};
+</script>
