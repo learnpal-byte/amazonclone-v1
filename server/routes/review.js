61302bec4328a08f9cd3d615962732e92a490cd3
Alfa <learnpal29@gmail.com>
2019-11-14 01:56:59 +0800

    61 & 62 Review POST/GET API

---
+++ server/routes/review.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const Review = require("../models/review");
+const Product = require("../models/product");
+const verifyToken = require("../middlewares/verify-token");
+const upload = require("../middlewares/upload-photo");
+
+/* Review POST */
+router.post(
+  "/reviews/:productID",
+  [verifyToken, upload.single("photo")],
+  async (req, res) => {
+    try {
+      const review = new Review();
+      review.headline = req.body.headline;
+      review.body = req.body.body;
+      review.rating = req.body.rating;
+      review.photo = req.file.location;
+      review.user = req.decoded._id;
+      review.productID = req.params.productID;
+
+      await Product.update({ $push: review._id });
+
+      const savedReview = await review.save();
+
+      if (savedReview) {
+        res.json({
+          success: true,
+          message: "Succesfully Added Review"
+        });
+      }
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: err.message
+      });
+    }
+  }
+);
+
+router.get("/reviews/:productID", async (req, res) => {
+  try {
+    const productReviews = await Review.find({
+      productID: req.params.productID
+    })
+      .populate("user")
+      .exec();
+
+    res.json({
+      success: true,
+      reviews: productReviews
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -34,11 +34,13 @@ const productRoutes = require("./routes/product");
 const categoryRoutes = require("./routes/category");
 const ownerRoutes = require("./routes/owner");
 const userRoutes = require("./routes/auth");
+const reviewRoutes = require("./routes/review");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", ownerRoutes);
 app.use("/api", userRoutes);
+app.use("/api", reviewRoutes);
 
 app.listen(3000, err => {
   if (err) {
